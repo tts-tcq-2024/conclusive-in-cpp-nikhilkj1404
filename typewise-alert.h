565a61dccbeed56a764aecc7ef7d6b967ee7ffa4
nikhilkj1404 <nikhilkj0414@gmail.com>
2024-08-21 21:51:33 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -1,32 +1,37 @@
 #pragma once
 
 typedef enum {
-  PASSIVE_COOLING,
-  HI_ACTIVE_COOLING,
-  MED_ACTIVE_COOLING
+    PASSIVE_COOLING,
+    HI_ACTIVE_COOLING,
+    MED_ACTIVE_COOLING
 } CoolingType;
 
 typedef enum {
-  NORMAL,
-  TOO_LOW,
-  TOO_HIGH
+    NORMAL,
+    TOO_LOW,
+    TOO_HIGH
 } BreachType;
 
+typedef struct {
+    int lowerLimit;
+    int upperLimit;
+} Limits;
+
 BreachType inferBreach(double value, double lowerLimit, double upperLimit);
+Limits getLimits(CoolingType coolingType);
 BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
 
 typedef enum {
-  TO_CONTROLLER,
-  TO_EMAIL
+    TO_CONTROLLER,
+    TO_EMAIL
 } AlertTarget;
 
 typedef struct {
-  CoolingType coolingType;
-  char brand[48];
+    CoolingType coolingType;
+    char brand[48];
 } BatteryCharacter;
 
-void checkAndAlert(
-  AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
-
+void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
+void sendAlert(AlertTarget alertTarget, BreachType breachType);
 void sendToController(BreachType breachType);
 void sendToEmail(BreachType breachType);
